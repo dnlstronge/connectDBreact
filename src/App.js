4aba1a62505baadba180c7285c7f8dd0354f7b87
Dan Stronge <dnlstronge@gmail.com>
2023-04-02 12:14:16 +0100

    [x] - add error handling

--- src/App.js
+++ src/App.js
@@ -7,23 +7,34 @@ import { useState } from "react";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isPending, setIsPending] = useState(false);
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(null)
 
   async function fetchMoviesHandler() {
-    setIsPending(!isPending);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setIsPending(false);
+    try {
+      setIsPending(!isPending);
+      setError(null) // clear prev errors
+      const response = await fetch("https://swapi.dev/api/film");
+      const data = await response.json();
+  
+
+      if(!response.ok) {
+        throw new Error(`Ooops, Error ${response.status}`)
+      }
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setIsPending(false);  
+    } catch (error) {
+      setError(error.message)
+    }
+    
   }
 
   return (
@@ -34,7 +45,8 @@ function App() {
       <section>
         {!isPending && movies.length > 0 && <MoviesList movies={movies} />}
         {!isPending && movies.length === 0 && <p>No movies found</p>}
-        {isPending && <p>LOADING...</p>}
+        {error && <p>{error}</p>}
+        {isPending && error && <p>LOADING...</p>}
       </section>
     </React.Fragment>
   );
