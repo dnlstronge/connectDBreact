4f60ee6da7b59b0127604aafd312edef6564b917
Dan Stronge <dnlstronge@gmail.com>
2023-04-02 11:46:12 +0100

    [x] - implement loading section

--- src/App.js
+++ src/App.js
@@ -6,28 +6,34 @@ import { useState } from "react";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isPending, setIsPending] = useState(false)
 
   async function fetchMoviesHandler() {
-   const response = await fetch("https://swapi.dev/api/films/")
-   const data = await response.json()
-  
-   const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date
-          };
-        });
-        setMovies(transformedMovies);
-      }
-  
+    setIsPending(!isPending)
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsPending(false)
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      {isPending && 
+      <section><div>
+        <p>LOADING...</p>
+      </div></section> }
       <section>
         <MoviesList movies={movies} />
       </section>
